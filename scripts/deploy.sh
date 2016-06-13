6ca2829d553fbc532f19a329e81382518a0d70fe
Antoine GIRARD <sapk@users.noreply.github.com>
2016-06-13 15:42:52 +0200

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,36 @@
 #!/bin/bash
 #Based on : https://gist.github.com/domenic/ec8b0fc8ab45f39403dd
-echo "TODO"
+
+set -e # Exit with nonzero exit code if anything fails
+
+SOURCE_BRANCH="master"
+TARGET_BRANCH="gh-pages"
+COMMIT_AUTHOR_EMAIL="travis@nobody.fr"
+KEYFILE="$TRAVIS_BUILD_DIR/keys/deploy_key"
+
+#Generate a key in needed that will be store in cache (and to be haded to github)
+if [ ! -f $KEYFILE ]; then
+    ssh-keygen -t rsa -b 4096 -C "$COMMIT_AUTHOR_EMAIL"  -N "" -f $KEYFILE
+fi
+cat "$KEYFILE.pub"
+
+# Pull requests and commits to other branches shouldn't try to deploy, just build to verify
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+    echo "Skipping deploy of gh-pages."
+    exit 0
+fi
+
+git config user.name "Travis CI"
+git config user.email "$COMMIT_AUTHOR_EMAIL"
+
+# Commit the "changes", i.e. the new version.
+# The delta will show diffs between new and old versions.
+git add .
+git commit -m "Deploy to GitHub Pages: ${TRAVIS_COMMIT}"
+
+chmod 600 $KEYFILE
+eval `ssh-agent -s`
+ssh-add $KEYFILE
+
+# Now that we're all set up, we can push.
+git push git@github.com:adentes-org/SOFIA.git $TARGET_BRANCH
