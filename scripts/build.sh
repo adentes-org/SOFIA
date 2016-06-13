4c8e0512c861704aad2cd1f86ebf4800d9ca6c87
Antoine GIRARD <sapk@sapk.fr>
2016-06-13 19:22:02 +0200

    Improve script

--- scripts/build.sh
+++ scripts/build.sh
@@ -14,18 +14,18 @@ export -f addFile
 echo "Getting webapp..."
 git clone https://github.com/adentes-org/SOFIA.git "web-app/" && cd web-app
 
-echo "Linking acah if exist..."
+echo "Linking dependances if exist..."
 
-if [ ! -d "../../node_modules" ]; then
-  ln -s "../../node_modules" "node_modules"
+if [ ! -d "$TRAVIS_BUILD_DIR/node_modules" ]; then
+  ln -s "$TRAVIS_BUILD_DIR/node_modules" "node_modules"
 fi
 
-if [ ! -d "../../bower_components" ]; then
-  ln -s "../../bower_components" "bower_components"
+if [ ! -d "$TRAVIS_BUILD_DIR/bower_components" ]; then
+  ln -s "$TRAVIS_BUILD_DIR/bower_components" "bower_components"
 fi
 
 echo "Building webapp..."
-npm install && bower-installer && gulp 
+npm install && bower-installer && gulp
 cd www
 
 addFile index.html
