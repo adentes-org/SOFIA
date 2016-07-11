4dd19df17836bd50dbf1b6e7ee66797ae24ec51b
Antoine GIRARD <sapk@users.noreply.github.com>
2016-07-11 10:01:31 +0200

    Define as a module

--- www/assets/platform/browser/init.js
+++ www/assets/platform/browser/init.js
@@ -1,16 +1,9 @@
-/* global $, Vue, requirejs */
+/* global Vue */
 'use strict';
-console.log("Init browser platform");
-
-// For testing purpose //
-Vue.config.debug = true;
-$("head").append('<link rel="stylesheet" type="text/css" href="dist/platform/android/css/style.css">');
-requirejs(['platform/android/init']);
-/////////////////////////
-
-/*
- S.platform = {
- isLoaded: true,
- id: browser
- }
- */
+define(["jquery", "platform/android/init"], function($,android) {
+	console.log("Init browser platform");
+	// For testing purpose //
+	Vue.config.debug = true;
+	$("head").append('<link rel="stylesheet" type="text/css" href="dist/platform/android/css/style.css">');
+	return android; //Load android interface in browser.
+});
