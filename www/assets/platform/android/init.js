bf6ce75b93fdd28b9e82e0c1c99aab6140584a4b
Antoine GIRARD <sapk@users.noreply.github.com>
2016-07-11 15:47:37 +0200

    Update init.js

--- www/assets/platform/android/init.js
+++ www/assets/platform/android/init.js
@@ -1,19 +1,15 @@
 /* global componentHandler, S, $, requirejs */
 'use strict';
-(function () {
+
+define(["jquery",'lib/material-design-lite/material.min', 'promise!platform/android/template'], function($,componentHandler,template) {
   //Vue.config.silent = true
   console.log("Init android platform");
 
   $("head").append('<link rel="stylesheet" type="text/css" href="dist/lib/material-design-lite/material.min.css">');
-
-  //$("head").append('<link rel="stylesheet" type="text/css" href="assets/css/platform/android.css">');
-
-  //$("head").append('<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">');
-  //$("body").append('<script src="assets/lib/material-design-lite/material.min.js"></script>');
-
-
-  requirejs(['lib/material-design-lite/material.min', 'promise!platform/android/template'], function () {
-      S.platform = {
+  $("head").append('<link rel="stylesheet" type="text/css" href="dist/lib/dialog-polyfill/dialog-polyfill.css">'); //Load style for dialog
+  $("head").append('<link rel="stylesheet" type="text/css" href="dist/platform/' + cordova.platformId + '/css/style.css">');
+  
+  var platform = {
           isLoaded: true,
           id: "android",
           events: {
@@ -27,5 +23,4 @@
               }
           }
       };
-  });
-}());
+});
