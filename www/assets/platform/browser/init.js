14e129cbe6726e28281b58fae2e89439cf328575
Antoine GIRARD <sapk@users.noreply.github.com>
2016-07-11 15:51:12 +0200

    Update init.js

--- www/assets/platform/browser/init.js
+++ www/assets/platform/browser/init.js
@@ -1,9 +1,5 @@
-/* global Vue */
 'use strict';
-define(["jquery", "platform/android/init"], function($,android) {
+define(["platform/android/init"], function($,android) {
 	console.log("Init browser platform");
-	// For testing purpose //
-	Vue.config.debug = true;
-	$("head").append('<link rel="stylesheet" type="text/css" href="dist/platform/android/css/style.css">');
 	return android; //Load android interface in browser.
 });
