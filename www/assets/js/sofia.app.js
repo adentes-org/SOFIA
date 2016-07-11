ae14f637c4749c31ef14f5040de958bc214949e4
Antoine GIRARD <sapk@users.noreply.github.com>
2016-07-11 10:33:19 +0200

    Update sofia.app.js

--- www/assets/js/sofia.app.js
+++ www/assets/js/sofia.app.js
@@ -1,43 +1,32 @@
-'use strict';
-var S = S || {};
 
-S.app = {
-    // Application Constructor
-    initialize: function () {
-        S.app.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function () {
-        //We use requirejs so everything is ready whenwe are here
-        //document.addEventListener('deviceready', this.onDeviceReady, false);
-        window.setTimeout(S.app.onDeviceReady, 1000);
-    },
-    removeLoader: function () {
-        $('body>.app-loading').remove();
-    },
-    onDeviceReady: function () {
-        $("body>.app").append(S.template.base());
-        S.vue.init();
-        //Vue = require('vue');
-        /*
-        new Vue({
-            el: '#indox',
-            data: {
-                fiches: [
-                    {id: 1},
-                    {id: 2},
-                    {id: 3},
-                ]
+'use strict';
+define([], function() {
+    var app = {
+        // Application Constructor
+        initialize: function () {
+            S.app.bindEvents();
+        },
+        // Bind Event Listeners
+        //
+        // Bind any events that are required on startup. Common events are:
+        // 'load', 'deviceready', 'offline', and 'online'.
+        bindEvents: function () {
+            //We use requirejs so everything is ready whenwe are here
+            //document.addEventListener('deviceready', this.onDeviceReady, false);
+            window.setTimeout(S.app.onDeviceReady, 1000); //TODO when everything is rework test to remove this timing
+        },
+        removeLoader: function () {
+            $('body>.app-loading').remove();
+        },
+        onDeviceReady: function () {
+            $("body>.app").append(S.template.base());
+            S.vue.init();
+            if(typeof S.platform.events.afterDeviceReady === "function"){
+                S.platform.events.afterDeviceReady();
             }
-        });
-        */
-        if(typeof S.platform.events.afterDeviceReady === "function"){
-            S.platform.events.afterDeviceReady();
+            $("body").removeClass("loading");
+            window.setTimeout(S.app.removeLoader, 1000);
         }
-        $("body").removeClass("loading");
-        window.setTimeout(S.app.removeLoader, 1000);
-    }
-};
+    };
+    return app;
+});
