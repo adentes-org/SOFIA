5b2d4efd5103be79d4ccac8df0a0bc4646ddd437
Antoine GIRARD <sapk@users.noreply.github.com>
2016-07-11 10:38:54 +0200

    Update main.js

--- www/assets/js/main.js
+++ www/assets/js/main.js
@@ -1,23 +1,38 @@
-  define(['jquery','i18n!app/nls/base', 'vue', 'vue-router', 'moment', 'moment-locales','app/sofia.polyfill'], function ($,lang, Vue, VueRouter, moment) {
-      
+  define(['jquery','i18n!app/nls/base', 'vue', 'vue-router', 'moment', 'moment-locales','app/sofia.tool','app/sofia.config','app/sofia.template','app/sofia.user','app/sofia.app','app/sofia.polyfill'], function ($,lang, Vue, VueRouter, moment,tool,config,template,user,app) {
+      //Define primary object 
       var S = {
         lang : lang
-        language : localStorage["sofia-language"] || navigator.language || navigator.userLanguage || 'en'
+        language : localStorage["sofia-language"] || navigator.language || navigator.userLanguage || 'en',
+        config : config,
+        tool:tool,
+        template:template,
+        user:user,
+        app:app
       };
       
       // Setup plugins
       Vue.use(VueRouter);
       moment.locale(S.language);
       
-      $("head").append('<link rel="stylesheet" type="text/css" href="dist/lib/dialog-polyfill/dialog-polyfill.css">'); //Load style for dialog
-      //$("head").append('<link rel="stylesheet" type="text/css" href="dist/lib/objectdiff/style.css">'); //Load style for diff
+      //window.S = S; //TODO check if usefull
+      window.setTimeout(S.app.initialize,250); // Add time if all not already loaded for safety //TODO everythings is now ready
 
-      requirejs(['pouchdb-authentication', 'app/sofia.tool', 'app/sofia.template', 'app/sofia.config'], function () {
-          $("head").append('<link rel="stylesheet" type="text/css" href="dist/platform/' + cordova.platformId + '/css/style.css">');
+      //Apply theming
+      //TODO This should be loaded by platform
+      $("head").append('<link rel="stylesheet" type="text/css" href="dist/lib/dialog-polyfill/dialog-polyfill.css">'); //Load style for dialog
+      $("head").append('<link rel="stylesheet" type="text/css" href="dist/platform/' + cordova.platformId + '/css/style.css">');
+
+      return S;
+      //TODO HERE
+      
+      /*
+
+      requirejs(['pouchdb-authentication', ], function () {
           requirejs(['platform/' + cordova.platformId + '/init', 'app/sofia.db'], function () {
-              requirejs(['app/sofia.vue', 'app/sofia.data', 'app/sofia.user', 'app/sofia.app'], function () {
+              requirejs(['app/sofia.vue', 'app/sofia.data', ], function () {
                   window.setTimeout(S.app.initialize,250); // Add time if all not already loaded for safety
               });
           });
       });
+      */
   });
